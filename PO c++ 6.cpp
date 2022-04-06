dc7c6f65ebd0741200e10229a5e303291b918fbf
I_am_not_a_weeb <kubutek04@gmail.com>
2022-04-06 17:04:50 +0200

    dodanie kopiator trojkata dodanie toStringi do wszystkich dotychczasowych funkcji dodanie klasy czworokat dodanie klasy obraz

--- PO c++ 6.cpp
+++ PO c++ 6.cpp
@@ -33,6 +33,10 @@ public:
     {
         return y;
     }
+    string toString()
+    {
+
+    }
 };
 /*class Wektor
 {
@@ -84,18 +88,22 @@ public:
     {
         return p2;
     }
+    string toString()
+    {
+
+    }
 };
 class Trojkat
 {
     Linia lin1, lin2, lin3;
-    Punkt Q;
+    Punkt Q;            /// srodek ciezkosci  trojkata, implementacja nie jest oparta na jego zmianie (jeszcze)
     //Wektor wp1, wp2, wp3;
 public:
-    Trojkat()           ///konstruktor domyslny
+    Trojkat()           /// konstruktor domyslny
     {
 
     }
-    Trojkat(Punkt fp1, Punkt fp2, Punkt fp3)            ///konstruktor tworzacy trojkat z trzech punktow, tymczasowa implementacja
+    Trojkat(Punkt fp1, Punkt fp2, Punkt fp3)            /// konstruktor tworzacy trojkat z trzech punktow, tymczasowa implementacja
     {
         lin1 = Linia(fp1, fp2); lin2 = Linia(fp2, fp3); lin3 = Linia(fp3, fp1);
         Q = Punkt((fp1.getX() + fp2.getX() + fp3.getX()) / 3, (fp1.getY() + fp2.getY() + fp3.getY()) / 3);
@@ -103,12 +111,23 @@ public:
         wp2 = Wektor(Q, fp2);
         wp3 = Wektor(Q, fp3);*/
     }
+    Trojkat(const Trojkat& old_tro)
+    {
+        lin1 = old_tro.lin1;
+        lin2 = old_tro.lin2;
+        lin3 = old_tro.lin3;
+        Q = old_tro.Q;
+    }
     void przesun(int dx, int dy)
     {
         Q = Punkt(Q.getX() + dx, Q.getY() + dy);
         lin1 = Linia(Punkt(lin1.getP1().getX() + dx,lin1.getP1().getY() + dy), Punkt(lin1.getP2().getX() + dx,lin1.getP2().getY()+dy));
         lin2 = Linia(Punkt(lin2.getP1().getX() + dx, lin2.getP1().getY() + dy), Punkt(lin2.getP2().getX() + dx, lin2.getP2().getY() + dy));
         lin3 = Linia(Punkt(lin3.getP1().getX() + dx, lin3.getP1().getY() + dy), Punkt(lin3.getP2().getX() + dx, lin3.getP2().getY() + dy));
+    }
+    string toString()
+    {
+
     }
     int wypiszQx()
     {
@@ -135,7 +154,50 @@ public:
         return lin.getP2().getY();
     }                                               /// tymczasowe / test.kon
 };
+class Czworokat
+{   
+    Linia lin1, lin2, lin3, lin4;
+    Punkt Q;
+public:
+    Czworokat()
+    {
 
+    }
+    Czworokat(Punkt fp1, Punkt fp2, Punkt fp3, Punkt fp4)   ///konieczne wpisanie w kolejnosci tworzenia linii obwodu
+    {
+        lin1 = Linia(fp1, fp2); 
+        lin2 = Linia(fp2, fp3); 
+        lin3 = Linia(fp3, fp4);
+        lin4 = Linia(fp4, fp1);
+        Q = Punkt((fp1.getX() + fp2.getX() + fp3.getX() + fp4.getX()) / 4, (fp1.getY() + fp2.getY() + fp3.getY() + fp4.getY()) / 3);
+    }
+    Czworokat(const Czworokat &old_czwo)
+    {
+
+    }
+    void przesun(int dx,int dy)
+    {
+        lin1 = Linia(Punkt(lin1.getP1().getX() + dx, lin1.getP1().getY() + dy), Punkt(lin1.getP2().getX() + dx, lin1.getP2().getY() + dy));
+        lin2 = Linia(Punkt(lin2.getP1().getX() + dx, lin2.getP1().getY() + dy), Punkt(lin2.getP2().getX() + dx, lin2.getP2().getY() + dy));
+        lin3 = Linia(Punkt(lin3.getP1().getX() + dx, lin3.getP1().getY() + dy), Punkt(lin3.getP2().getX() + dx, lin3.getP2().getY() + dy));
+        lin4 = Linia(Punkt(lin4.getP1().getX() + dx, lin4.getP1().getY() + dy), Punkt(lin4.getP2().getX() + dx, lin4.getP2().getY() + dy));
+    }
+    string toString()
+    {
+
+    }
+};
+class Obraz
+{
+    Trojkat trojkaty;
+    Czworokat czworokaty;
+public:
+    Obraz()
+    {
+
+    }
+
+};
 int main()
 {
     Punkt a(-3, 2);
